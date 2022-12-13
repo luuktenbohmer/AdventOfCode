d6e802ecde74273cb42ef4b59c93dc4bea605235
Luuk ten Böhmer <l.ten.bohmer@betabit.nl>
2022-12-13 10:33:47 +0100

    Day 13

---
+++ Day13/Items.cs
@@ -0,0 +1,171 @@
+﻿using System.Diagnostics;
+
+namespace Day13;
+
+public class Packet
+{
+    public Packet(string rawPacket)
+    {
+        Parse(rawPacket);
+        RawPacket = rawPacket;
+    }
+
+    private void Parse(string rawPacket)
+    {
+        Items = new();
+        ParseInternal(rawPacket[1..], Items);
+    }
+
+    private void ParseInternal(string rawPacket, ListItem? currentList)
+    {
+        if (currentList == null)
+        {
+            if (rawPacket.Length != 0)
+                throw new UnreachableException();
+
+            return;
+        }
+        var listOpenIndex = rawPacket.IndexOf('[');
+        var listCloseIndex = rawPacket.IndexOf(']');
+
+        // Both -1 -> End of string
+        if (listOpenIndex == listCloseIndex)
+            return;
+
+        if ((listOpenIndex < listCloseIndex && listOpenIndex != -1) || listCloseIndex == -1)
+        {
+            var integers = rawPacket[..listOpenIndex]
+                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Select(i => new IntegerItem(i));
+
+            currentList.Items.AddRange(integers);
+
+            var newList = new ListItem();
+            currentList.Items.Add(newList);
+            newList.Parent = currentList;
+            ParseInternal(rawPacket[(listOpenIndex + 1)..], newList);
+        }
+        else if ((listCloseIndex < listOpenIndex && listCloseIndex != -1) || listOpenIndex == -1)
+        {
+            var integers = rawPacket[..listCloseIndex]
+                .Split(',', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Select(i => new IntegerItem(i));
+
+            currentList.Items.AddRange(integers);
+
+            ParseInternal(rawPacket[(listCloseIndex + 1)..], currentList.Parent);
+        }
+        else
+        {
+            throw new UnreachableException($"Should not happen. Open: {listOpenIndex}, Close: {listCloseIndex}");
+        }
+    }
+
+    public bool GreaterThan(Packet other) =>
+        Items.GreaterThan(other.Items);
+
+    public ListItem Items { get; set; } = new();
+    public string RawPacket { get; }
+}
+
+public abstract class Item
+{
+    public ListItem? Parent { get; set; }
+    public abstract bool GreaterThan(Item other);
+    public abstract bool SimilarTo(Item other);
+}
+
+public class ListItem : Item
+{
+    public List<Item> Items { get; set; } = new();
+
+    public override bool GreaterThan(Item other)
+    {
+        if (other is ListItem listOther)
+        {
+            for (int i = 0; i < Math.Min(Items.Count, listOther.Items.Count); i++)
+            {
+                if (Items[i].SimilarTo(listOther.Items[i]))
+                    continue;
+
+                return Items[i].GreaterThan(listOther.Items[i]);
+            }
+
+            return Items.Count > listOther.Items.Count;
+        }
+        else if (other is IntegerItem)
+        {
+            return GreaterThan(new ListItem { Items = new() { other } });
+        }
+
+        throw new UnreachableException();
+    }
+
+    public override bool SimilarTo(Item other)
+    {
+        if (other is ListItem listOther)
+        {
+            for (int i = 0; i < Math.Min(Items.Count, listOther.Items.Count); i++)
+            {
+                if (Items[i].SimilarTo(listOther.Items[i]))
+                    continue;
+
+                return false;
+            }
+
+            return Items.Count == listOther.Items.Count;
+        }
+        else if (other is IntegerItem)
+        {
+            return SimilarTo(new ListItem { Items = new() { other } });
+        }
+
+        throw new UnreachableException();
+    }
+}
+
+public class IntegerItem : Item
+{
+    public IntegerItem(int value)
+    {
+        Value = value;
+    }
+
+    public int Value { get; }
+
+    public override bool GreaterThan(Item other)
+    {
+        if (other is IntegerItem integerOther)
+        {
+            return Value > integerOther.Value;
+        }
+        else if (other is ListItem listOther)
+        {
+            return new ListItem
+            {
+                Items = new() { new IntegerItem(Value) }
+            }.GreaterThan(listOther);
+        }
+
+        throw new UnreachableException();
+    }
+
+    public override bool SimilarTo(Item other)
+    {
+        if (other is IntegerItem integerOther)
+        {
+            return Value == integerOther.Value;
+        }
+        else if (other is ListItem listOther)
+        {
+            return new ListItem
+            {
+                Items = new() { new IntegerItem(Value) }
+            }.SimilarTo(listOther);
+        }
+
+        throw new UnreachableException();
+    }
+}
---
+++ Day13/Program.cs
@@ -0,0 +1,57 @@
+﻿using Day13;
+
+var linesP1 = await File.ReadAllLinesAsync("Input/input.txt");
+
+var packetsP1 = new List<(Packet, Packet)>();
+for (int i = 0; i < linesP1.Length; i += 3)
+{
+    packetsP1.Add((new(linesP1[i]), new(linesP1[i + 1])));
+}
+
+// P1
+var resultP1 = 0;
+Console.Write("Right order: ");
+for (int i = 0; i < packetsP1.Count; i++)
+{
+    if (packetsP1[i].Item1.GreaterThan(packetsP1[i].Item2))
+        continue;
+
+    Console.Write($"{i + 1},");
+    resultP1 += i + 1;
+}
+
+Console.WriteLine();
+Console.WriteLine("P1: " + resultP1);
+
+// P2
+var linesP2 = linesP1
+    .Where(l => !string.IsNullOrEmpty(l))
+    .ToList();
+var divider1 = "[[2]]";
+var divider2 = "[[6]]";
+linesP2.Add(divider1);
+linesP2.Add(divider2);
+
+var packetsP2 = linesP2
+    .Select(l => new Packet(l))
+    .ToList();
+
+var ordered = packetsP2.Order(new PacketComparer()).ToList();
+var i1 = ordered.FindIndex(p => p.RawPacket == divider1) + 1;
+var i2 = ordered.FindIndex(p => p.RawPacket == divider2) + 1;
+
+Console.WriteLine("P2: " + i1 * i2);
+Console.ReadKey();
+
+public class PacketComparer : IComparer<Packet>
+{
+    public int Compare(Packet? x, Packet? y)
+    {
+        if (x.GreaterThan(y))
+        {
+            return 1;
+        }
+
+        return -1;
+    }
+}
