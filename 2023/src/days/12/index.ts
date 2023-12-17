e9ed1090c84e323d45134b205579e52234a0647d
Luuk ten Böhmer <l.ten.bohmer@betabit.nl>
2023-12-17 12:57:52 +0100

    Day 12 part 2

--- 2023/src/days/12/index.ts
+++ 2023/src/days/12/index.ts
@@ -1,53 +1,117 @@
-const isPossible = (springs: string, groups: Groups): boolean => {
-  const foundGroups: number[] = [];
-  let currentGroup = 0;
-  for (const spring of springs) {
-    if (spring === "#") {
-      currentGroup++;
-    } else {
-      if (currentGroup > 0) {
-        foundGroups.push(currentGroup);
+type PossibleGroup = {
+  /** Start index of group */
+  startIndex: number;
+  /** Total length */
+  length: number;
+  /** Start count of unused places */
+  current: number;
+  /** Known spring indexes relative to the group start */
+  known: number[];
+};
+
+const getPossibleGroups = (records: string): PossibleGroup[] => {
+  let currentGroup: PossibleGroup | undefined;
+  const possibleGroups = [];
+  for (let i = 0; i < records.length; i++) {
+    if (records[i] === ".") {
+      currentGroup = undefined;
+      continue;
+    }
+
+    if (!currentGroup) {
+      currentGroup = {
+        startIndex: i,
+        current: 0,
+        length: 0,
+        known: [],
+      };
+      possibleGroups.push(currentGroup);
+    }
+
+    currentGroup.length++;
+    if (records[i] === "#") {
+      currentGroup.known.push(i - currentGroup.startIndex);
+    }
+  }
+
+  return possibleGroups;
+};
+
+const internalSolve = (possibleGroups: PossibleGroup[], groups: number[], results: Record<string, number>): number => {
+  // return cached result if already calculated.
+  const resultKey = `${possibleGroups.length}_${groups.length}_${possibleGroups[0]?.current}`;
+  if (results[resultKey] !== undefined) {
+    return results[resultKey];
+  }
+
+  const group = groups[0];
+  let placeCount = 0;
+  let skipped = false;
+  while (possibleGroups.length > 0) {
+    const possibleGroup = possibleGroups[0];
+    for (let i = possibleGroup.current; i < possibleGroup.length; i++) {
+      if (possibleGroup.known.some((k) => k === i - 1)) {
+        skipped = true;
+        break;
+      }
+      // Not enough space left in group.
+      if (possibleGroup.length - i < group) {
+        break;
       }
 
-      currentGroup = 0;
+      // If the next place is a known spring, then continue.
+      if (possibleGroup.known.find((k) => k === i + group)) {
+        continue;
+      }
+
+      // Place found.
+      if (groups.length === 1) {
+        // The last group was placed.
+
+        // Continue if known spring is missed.
+        if (
+          possibleGroups.slice(1).some((g) => g.known.length > 0) ||
+          possibleGroup.known.some((k) => k >= i + group)
+        ) {
+          continue;
+        }
+
+        // All groups were placed correctly.
+        placeCount++;
+      } else {
+        // Place next group(s) recursively.
+        placeCount += internalSolve(
+          [{ ...possibleGroup, current: i + group + 1 }, ...possibleGroups.slice(1)],
+          groups.slice(1),
+          results
+        );
+      }
     }
-  }
 
-  if (currentGroup > 0) {
-    foundGroups.push(currentGroup);
-  }
-
-  return (
-    foundGroups.length === groups.groups.length && foundGroups.every((group, index) => group === groups.groups[index])
-  );
-};
-
-const fillSprings = (springs: string, groups: Groups, index: number) => {
-  if (springs.split("#").length - 1 === groups.total) {
-    return isPossible(springs, groups) ? 1 : 0;
-  }
-
-  for (let i = index; i < springs.length; i++) {
-    if (springs[i] === "?") {
-      const yesSpring = springs.replaceAt(i, "#");
-      const noSpring = springs.replaceAt(i, ".");
-      return fillSprings(yesSpring, groups, i + 1) + fillSprings(noSpring, groups, i + 1);
+    // A known spring couldn't be matched in this branch.
+    if (possibleGroup.known.some((known) => known >= possibleGroup.current) || skipped) {
+      break;
     }
+
+    // Remove group if no place was left.
+    possibleGroups = possibleGroups.slice(1);
   }
 
-  return 0;
-};
+  // Cache result.
+  results[resultKey] = placeCount;
 
-type Groups = { groups: number[]; total: number };
+  return placeCount;
+};
 
 export const solve = (input: string[]) => {
   return input
     .map((line) => {
       const splitLine = line.split(" ");
-      const springs = splitLine[0];
+      const records = splitLine[0];
       const groups = splitLine[1].split(",").map(Number);
 
-      return fillSprings(springs, { groups, total: groups.sum() }, 0);
+      const possibleGroups = getPossibleGroups(records);
+      return internalSolve(possibleGroups, groups, {});
     })
     .sum();
 };
@@ -56,10 +120,11 @@ export const solve2 = (input: string[]) => {
   return input
     .map((line) => {
       const splitLine = line.split(" ");
-      const springs = Array(5).fill(splitLine[0]).join("?");
+      const records = Array(5).fill(splitLine[0]).join("?");
       const groups = Array(5).fill(splitLine[1]).join(",").split(",").map(Number);
 
-      return 0;
+      const possibleGroups = getPossibleGroups(records);
+      return internalSolve(possibleGroups, groups, {});
     })
     .sum();
 };
